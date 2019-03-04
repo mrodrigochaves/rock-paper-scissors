62c3933a81da94cb65f40ca0bd36f60273277f75
mrodrigochaves <marciorodrigosp@gmail.com>
2019-03-04 05:12:15 -0300

    Finishing the project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rock-paper-scissors
-This is a game project The Odin Project
+# The Rock Paper Scissors Game
+
+- This is a game project The Odin Project.
+- A simple implementation of the classic "rock paper scissors".
+- It is played only on the browser console.
--- app.js
+++ app.js
@@ -1,33 +1,81 @@
-
 // This funciton return a called randomly
 function computerPlay() {
-  const option = ["r", "p", "s"];
+  const option = ["rock", "paper", "scissor"];
   const getRandom = Math.floor(Math.random() * 3);
   return option[getRandom];
 }
 
 // This function plays a single round
 function playRound(playerSelection, computerSelection) {
-	switch (playerSelection + computerSelection) {
-    case "rs":
-    case "pr":
-    case "sp":
-      console.log("User Wins!");
+  switch (playerSelection + computerSelection) {
+    case "rockscissor":
+    case "paperrock":
+    case "scissorpaper":
+      playerScore++;
+      console.log(`User Wins! ${playerSelection} beats ${computerSelection}`);
       break;
-    case "rp":
-    case "ps":
-    case "sr":
-      console.log("User Loses!");
+    case "rockpaper":
+    case "paperscissor":
+    case "scissorrock":
+      computerScore++;
+      console.log(`User Loses! ${computerSelection} beats ${playerSelection}`);
       break;
-    case "rr":
-    case "pp":
-    case "ss":
+    case "rockrock":
+    case "paperpaper":
+    case "scissorscissor":
+      drawScore++;
       console.log("It's a draw!");
       break;
   }
 }
 
-const playerSelection = 'r'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+// This get user and computer values
+function getValue() {
+  playerSelection = prompt(`Round:${round} - Rock, paper or scissor`);
 
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerPlay();
+}
+
+// This function game
+function game() {
+  playerScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+  round = 0;
+
+  // This run five rounds
+  while (round < 5) {
+    getValue();
+
+    // This call game rounds
+    playRound(playerSelection, computerSelection);
+    round++;
+  }
+
+  // This return text in green color
+  console.log("%c Final Score!", "color: #00ff00");
+  console.log("%c *****************************", "color: #00ff00");
+
+  // This return the winner of game based on their highest points
+  if (playerScore > computerScore) {
+    console.log(
+      `You are the winner with a score of ${playerScore} to ${computerScore}, and ${drawScore} for draw!`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `Computer is the winner with a score of ${computerScore} to ${playerScore}, and ${drawScore} for draw!`
+    );
+  } else {
+    console.log(
+      `This is a drawn with a ${drawScore} draws, ${playerScore} for player and ${computerScore} for computer!`
+    );
+  }
+  console.log("\n");
+}
+
+let playerSelection, computerSelection;
+let playerScore, computerScore, drawScore;
+let round;
+
+console.log(game());
